61efbea2b8368fcb4e1dd193cf5e95c36187ca98
Dan Moran <danmoran@broadinstitute.org>
2019-02-04 13:42:46 -0500

    DSPDC-118 Fix build-docker logic.

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 # This program will read a list of Scala and SBT versions from build config
 # files. For each pair of versions, it will determine if a version already exists
@@ -10,98 +12,91 @@
 # only the major version (8) is specified for JDK and the script will get whatever the
 # latest version 8 of JDK exists at the time of build.
 
-FORCE=${FORCE:-0}
+declare -r FORCE=${FORCE:-0}
+declare -r REPO="broadinstitute/scala"
+declare -r JDK_VERSION=8
 
-# Docker Hub container name
-REPO="broadinstitute/scala"
+declare -r SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+declare -r SCALA_CONFIG=${SCRIPT_DIR}/scala-list.cfg
+declare -r SBT_CONFIG=${SCRIPT_DIR}/sbt-list.cfg
+declare -r DOCKERFILE_TEMPLATE=${SCRIPT_DIR}/Dockerfile.tmpl
+declare -r DOCKERFILE=${SCRIPT_DIR}/Dockerfile
 
-# Hard-code to JDK 8
-JDK_VERSION=8
+declare -r VALID_VERSION_PATTERN='^[a-zA-Z0-9]+'
 
-# Files containing version lists
-SCALA_CONFIG="scala-list.cfg"
-SBT_CONFIG="sbt-list.cfg"
+function check_config () {
+  local -r config=$1
 
-# Generic error outputting function
-errorout() {
-   if [ $1 -ne 0 ];
-        then
-        echo "${2}"
-        exit $1
-    fi
-}
-
-for config in "$SCALA_CONFIG" "$SBT_CONFIG"; do
   if [ -f "$config" ]; then
-    if [ ! egrep -q "^[a-zA-Z0-9]+" "$config" ]; then
-      errorout 1 "No valid entries in config file: ${config}"
+    if ! grep -Eq ${VALID_VERSION_PATTERN} ${config}; then
+      2>&1 echo No valid entries in config file: ${config}
+      exit 1
     fi
   else
-    errorout 1 "Missing version list: ${config}"
+    2>&1 echo Missing version list: ${config}
+    exit 1
   fi
-done
+}
 
-egrep "^[a-zA-Z0-9]+" "$SCALA_CONFIG" | while read scala_version rest; do
-  egrep "^[a-zA-Z0-9]+" "$SBT_CONFIG" | while read sbt_version rest; do
-    docker_version="scala-${scala_version}-sbt-${sbt_version}"
-    build_version=1
+function check_should_build () {
+  local -r docker_tag=$1
 
-    # Ensure Dockerfile doesn't exist
-    rm -f Dockerfile
+  2>&1 echo Checking if image ${docker_tag} needs to be built...
 
-    echo "Checking if Scala version (${scala_version}) on SBT version (${sbt_version}) needs to be built..."
-    if [ "$FORCE" -ne "1" ]; then
-      # See if version exists on docker hub
-      docker pull ${REPO}:${docker_version}
-      retcode=$?
+  if ! docker pull ${REPO}:${docker_tag}; then
+    return 0
+  else
+    2>&1 echo Image already built, skipping
+    return 1
+  fi
+}
 
-      if [ "$retcode" -eq "0" ]; then
-        echo "Skipping version"
-        build_version=0
-      fi
-    fi
+function build_docker () {
+  local -r scala_version=$1 sbt_version=$2
+  local -r docker_tag=scala-${scala_version}-sbt-${sbt_version}
 
-    if [ "$build_version" -eq "1" ]; then
-      echo "Building docker (${REPO}:${docker_version})"
+  if [ ${FORCE} -eq 1 ] || check_should_build ${docker_tag}; then
+    2>&1 echo Building docker ${REPO}:${docker_tag}
 
-      # Build Dockerfile from template
-      sed -e "s;SCALA_VERSION_TAG;${scala_version};" -e "s;SBT_VERSION_TAG;${sbt_version};" -e "s;JDK_VERSION_TAG;${JDK_VERSION};" < Dockerfile.tmpl > Dockerfile
+    # Build Dockerfile from template
+    sed -e "s;SCALA_VERSION_TAG;${scala_version};" \
+        -e "s;SBT_VERSION_TAG;${sbt_version};" \
+        -e "s;JDK_VERSION_TAG;${JDK_VERSION};" \
+        < ${DOCKERFILE_TEMPLATE} > ${DOCKERFILE}
 
-      # Add some Jenkins labels to designate this build
-      echo "LABEL GIT_BRANCH=${GIT_BRANCH}" >> Dockerfile
-      echo "LABEL GIT_COMMIT=${GIT_COMMIT}" >> Dockerfile
-      echo "LABEL BUILD_URL=${BUILD_URL}" >> Dockerfile
+    # Add some Jenkins labels to designate this build
+    echo LABEL GIT_BRANCH=${GIT_BRANCH} >> ${DOCKERFILE}
+    echo LABEL GIT_COMMIT=${GIT_COMMIT} >> ${DOCKERFILE}
+    echo LABEL BUILD_URL=${BUILD_URL} >> ${DOCKERFILE}
 
-      docker build --pull -t ${REPO}:${docker_version} .
-      retcode=$?
-      errorout $retcode "Failed to build docker image"
+    docker build --pull -t ${REPO}:${docker_tag} .
 
-      echo "Tagging docker (${REPO}:${docker_version}_${BUILD_NUMBER})"
-      docker tag ${REPO}:${docker_version} ${REPO}:${docker_version}_${BUILD_NUMBER}
-      retcode=$?
-      errorout $retcode "Build successful but could not tag to build number"
+    2>&1 echo Tagging docker ${REPO}:${docker_tag}_${BUILD_NUMBER}
+    docker tag ${REPO}:${docker_tag} ${REPO}:${docker_tag}_${BUILD_NUMBER}
 
-      echo "Pushing docker (${REPO}:${docker_version})"
-      docker push ${REPO}:${docker_version}
-      retcode=$?
-      errorout $retcode "Failed to push docker image"
+    2>&1 echo Pushing docker ${REPO}:${docker_tag}
+    docker push ${REPO}:${docker_tag}
 
-      echo "Pushing docker (${REPO}:${docker_version}_${BUILD_NUMBER})"
-      docker push ${REPO}:${docker_version}_${BUILD_NUMBER}
-      retcode=$?
-      errorout $retcode "Failed to push docker tag image"
+    2>&1 echo Pushing docker ${REPO}:${docker_tag}_${BUILD_NUMBER}
+    docker push ${REPO}:${docker_tag}_${BUILD_NUMBER}
 
-      # Clean up all pulled built images
-      echo "Cleaning up pulled and built images"
-      docker rmi ${REPO}:${docker_version}
-      retcode=$?
-      docker rmi ${REPO}:${docker_version}_${BUILD_NUMBER}
-      retcode=$(($retcode + $?))
-      errorout $retcode "Some images were not able to be cleaned up"
-    fi
+    2>&1 echo Cleaning up pulled and built images
+    docker rmi ${REPO}:${docker_tag} ${REPO}:${docker_tag}_${BUILD_NUMBER}
+  fi
+}
+
+function main () {
+  for config in ${SCALA_CONFIG} ${SBT_CONFIG}; do
+    check_config ${config}
   done
-done
 
-test -f Dockerfile && rm -f Dockerfile
+  trap "rm -f ${DOCKERFILE}" EXIT
 
-exit 0
+  grep -E ${VALID_VERSION_PATTERN} ${SCALA_CONFIG} | while read scala_version; do
+    grep -E ${VALID_VERSION_PATTERN} ${SBT_CONFIG} | while read sbt_version; do
+      build_docker ${scala_version} ${sbt_version}
+    done
+  done
+}
+
+main
